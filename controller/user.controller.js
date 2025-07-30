d0d0aac348c63ea805336a95d03c91d4b6c49875
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-07-30 16:31:06 +1200

    test

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,6 +8,9 @@ userController.createUser = async (req, res)=>{
     try{
         // req 정보는 어디서 온다? req.body에서 온다.
         const {email, name, password} = req.body // 데이터 가져오기
+        if (!email || !name || !password) {
+            return res.status(400).json({ status: "fail", message: "모든 필드를 입력해주세요." });
+        }
         const user = await User.findOne({email}) // DB에서 이미 존재하는 email인지 확인해주세요. 
         
         // 만약 유저가 이미 있다면
@@ -27,7 +30,7 @@ userController.createUser = async (req, res)=>{
         console.log("유저가 생성되었습니다.")
 
     }catch(error){
-        res.status(400).json({status: "fail", error})
+        res.status(400).json({ status: "fail", message: error.message || "회원가입 중 오류가 발생했습니다." });
 
     }
 }
@@ -36,7 +39,9 @@ userController.loginWithEmail= async (req, res)=>{
     try{
         const{email, password} = req.body
         const user = await User.findOne({email}, "-createdAt -updatedAt -__v")
-
+        if (!email || !password) {
+            return res.status(400).json({ status: "fail", message: "이메일과 비밀번호를 모두 입력해주세요." });
+        }
         if(user){
             // password=> 유저가 입력한 값 자체
             // user.password => 암호화 되어있음
@@ -51,7 +56,7 @@ userController.loginWithEmail= async (req, res)=>{
         }
         throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.")
     }catch(error){
-        res.status(400).json({status: "fail", message: error.message})
+        res.status(400).json({ status: "fail", message: error.message || "로그인 중 오류가 발생했습니다." });
     }
 }
 module.exports = userController
